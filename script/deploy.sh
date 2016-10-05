e3e706353f32775754f1c42fa40e6d8b782ca402
Jacob Paul <jacob@jacobpaul.io>
2016-10-05 15:47:09 -0500

    Avoid committing new files in deploy script
    CircleCI installs bundled gems into vendor

--- script/deploy.sh
+++ script/deploy.sh
@@ -9,7 +9,7 @@ git submodule update --recursive --remote
 if [ -n "$(git status --porcelain)" ]; then
   git config --global user.email "quesobot@users.noreply.github.com"
   git config --global user.name "quesobot"
-  git add .
+  git add --update
   git commit -m "Update gh-pages submodules"
   git push https://$QUESOBOT_ACCESS_TOKEN@github.com/cityofaustin/pages.git gh-pages
 fi
