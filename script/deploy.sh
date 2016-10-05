db25172503581a4e2b080da63e5fa8483aa29ec3
Jacob Paul <jacob@jacobpaul.io>
2016-10-05 15:16:08 -0500

    Add CircleCI configuration and deploy script (resolves #9)
    Sets us up to use CircleCI to automatically update the gh-pages branch
    with the latest submodules when a submodule repo webhook kicks off a
    build. The gh-pages commits updating the submodule SHAs are written by
    @quesobot, our new GitHub robot.
    
    (@quesobot has been granted write access to this repo, and a personal
    access token with `public_repo` scope is set to QUESOBOT_ACCESS_TOKEN
    in CirceCI.)
    
    At the moment, GitHub will fire a submodule repo webhook for every
    push event, including on non-master branches, so we may implement a
    webhook middleman (probably an AWS Lambda function) to avoid
    unnecessary builds.

---
+++ script/deploy.sh
@@ -0,0 +1,15 @@
+cd $HOME/pages
+
+git checkout -b gh-pages origin/gh-pages
+
+git submodule sync --recursive
+git submodule init
+git submodule update --recursive --remote
+
+if [ -n "$(git status --porcelain)" ]; then
+  git config --global user.email "quesobot@users.noreply.github.com"
+  git config --global user.name "quesobot"
+  git add .
+  git commit -m "Update gh-pages submodules"
+  git push https://$QUESOBOT_ACCESS_TOKEN@github.com/cityofaustin/pages.git gh-pages
+fi
