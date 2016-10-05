5449c5bb8331f4c4a01a0bcfd4b8c9c4709bcf3b
Jacob Paul <jacob@jacobpaul.io>
2016-10-05 15:55:57 -0500

    Update deploy script to merge master into gh-pages

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,14 +1,16 @@
 cd $HOME/pages
 
+git config --global user.email "quesobot@users.noreply.github.com"
+git config --global user.name "quesobot"
+
 git checkout -b gh-pages origin/gh-pages
+git merge master
 
 git submodule sync --recursive
 git submodule init
 git submodule update --recursive --remote
 
 if [ -n "$(git status --porcelain)" ]; then
-  git config --global user.email "quesobot@users.noreply.github.com"
-  git config --global user.name "quesobot"
   git add --update
   git commit -m "Update gh-pages submodules"
   git push https://$QUESOBOT_ACCESS_TOKEN@github.com/cityofaustin/pages.git gh-pages
